dc6971132ecba314853eea7e27b338fcb4f35d87
yijing0310 <n112140@gmail.com>
2025-01-10 22:52:40 +0800

    Add files via upload

---
+++ MFEE59MID-main/pages/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+const DB_HOST = '127.0.0.1';
+const DB_USER = 'root';
+const DB_PASS = 'P@ssw0rd';
+const DB_NAME = 'shin01';
+const DB_PORT = 3306;
+
+
+const PATH_BASE = '/
+期中專題';
---
+++ MFEE59MID-main/pages/index_.php
@@ -0,0 +1,9 @@
+<!-- 要補資料庫連線資料 -->
+<?php include __DIR__ . '/includes/html-header.php'; ?>
+<?php include __DIR__ . '/includes/html-sidebar.php'; ?>
+<?php include __DIR__ . '/includes/html-layout-navbar.php'; ?>
+<?php include __DIR__ . '/includes/html-content wrapper-start.php'; ?>
+<h2>這裡寫內容</h2>
+
+<?php include __DIR__ . '/includes/html-content wrapper-end.php'; ?>
+<?php include __DIR__ . '/includes/html-footer.php'; ?>
